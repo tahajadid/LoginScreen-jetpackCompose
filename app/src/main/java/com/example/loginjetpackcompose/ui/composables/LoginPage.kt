e1fb9fdd3ba8fedc15e61ce6c599cdb6cf0bd4ca
Taha Jadid <jadid.taha@waroom.com>
2023-04-25 11:26:02 +0100

    Change Main

---
+++ app/src/main/java/com/example/loginjetpackcompose/ui/composables/LoginPage.kt
@@ -0,0 +1,26 @@
+package com.example.loginjetpackcompose.ui.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import com.example.loginjetpackcompose.R
+
+@Composable
+fun LoginPage() {
+    val imageTop = painterResource(id = R.drawable.login_top)
+
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White),
+            contentAlignment = Alignment.TopCenter
+        ) {
+        }
+    }
+}
